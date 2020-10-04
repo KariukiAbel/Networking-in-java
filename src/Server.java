bdb92ccf3cba01e4b0b4ff66fd5d98c1a4659274
KariukiAbel <abelmuriithi@gmail.com>
2020-10-04 12:44:47 +0300

    This is a java class file to 'create' a server. Finished the constructor and the runServer methods.Pushing on to the next item.

---
+++ src/Server.java
@@ -0,0 +1,61 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends JFrame {
+    private JTextField enterField;
+    private JTextArea displayArea;
+    private ObjectInputStream inputStream; //input stream from client
+    private ObjectOutputStream outputStream; //output stream to client
+    private ServerSocket serverSocket;
+    private Socket connection;
+    private  int counter = 1; //counter of number of connections
+
+    public Server(){
+        super("Server");
+        enterField = new JTextField();
+        enterField.setEditable(false);
+        enterField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //send message to client
+                sendData(e.getActionCommand());
+                enterField.setText("");
+            }
+        });
+
+        add(enterField, BorderLayout.NORTH);
+        displayArea = new JTextArea();
+        add(new JScrollPane(displayArea),BorderLayout.CENTER);
+        setSize(300,150);
+        setVisible(true);
+        }
+
+        //set up and run server
+    public void runServer(){
+        try {
+            serverSocket = new ServerSocket(12345, 100); //create ServerSocket
+
+            while (true){
+                try {
+                    waitForConnection();
+                    getStreams(); //get input and output streams
+                    processConnection();
+                }catch (EOFException eofException){
+                    displayMessage("\nServer terminated connection");
+                }
+                finally {
+                    closeConnection();
+                    ++counter;
+                }
+            }
+        }catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+}
