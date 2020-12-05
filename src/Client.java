a1deef5d6a74074bd679c5c45ff2e8aede3c6c43
KariukiAbel <abelmuriithi@gmail.com>
2020-12-05 18:55:02 +0300

    Had to re-write the whole class

--- src/Client.java
+++ src/Client.java
@@ -2,84 +2,144 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.EOFException;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-public class Client extends JFrame {
+public class Client extends JFrame{
     private JTextField enterField;
     private JTextArea displayArea;
-    private DatagramSocket socket; //socket to connect to server
+    private ObjectOutputStream output;
+    private ObjectInputStream input;
+    private String message = ""; //message from server
+    private String chatServer; //host server for this application
+    private Socket client; //socket to communicate with the server
 
-    //set up GUI and DatagramSocket
-    public Client(){
+    //initiate chatServer and set up GUI
+    public Client(String host){
         super("Client");
-
-        enterField = new JTextField("Type message here ");
+        chatServer = host; //set server to which this client connects to
+        enterField = new JTextField();
+        enterField.setEditable(false);
         enterField.addActionListener(new ActionListener() {
+            //send message to server
             @Override
             public void actionPerformed(ActionEvent e) {
-                try{ //create and send packets
-                    //get message from textField
-                    String message = e.getActionCommand();
-                    displayArea.append("\nSending packet containing: " + message + "\n");
-                    byte [] data = message.getBytes(); //convert to bytes
-
-                    //create send packet
-                    DatagramPacket sendPacket = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), 5000);
-                    socket.send(sendPacket); //send packet
-                    displayArea.append("Packet sent\n");
-                    displayArea.setCaretPosition(displayArea.getText().length());
-                }catch (IOException ioException){
-                    displayMessage(ioException + "\n");
-                    ioException.printStackTrace();
-                }
+                sendData(e.getActionCommand());
+                enterField.setText("");
             }
         });
+
         add(enterField, BorderLayout.NORTH);
         displayArea = new JTextArea();
         add(new JScrollPane(displayArea), BorderLayout.CENTER);
-        setSize(400, 300);
+        setSize(300, 150);
         setVisible(true);
+    }
 
-        try { //create DatagramSocket for sending and receiving packets
-            socket = new DatagramSocket();
-        }catch (SocketException socketException){
-            socketException.printStackTrace();
-            System.exit(1);
+    //connect to server and process messages from server
+    public void runClient(){
+        try{ //connect to server, get streams, process connection
+            connectToServer(); // create a Socket to connection
+            getStreams(); //get the input and output streams
+            processConnection(); //process connection
+        }catch (EOFException eofException){
+            displayMessage("\nClient terminated connection");
+        }catch (IOException ioException){
+            ioException.printStackTrace();
+        }finally {
+            closeConnection();
         }
     }
 
-    //wait for packets to arrive from server, display packet contents
-    public void waitForPackets(){
-        while (true){
-            try { //receive packets and display contents
-                byte[] data = new byte[100];
-                DatagramPacket receivePacket = new DatagramPacket(data, data.length);
-                socket.receive(receivePacket); //wait for packet
+    //connect to server
+    private void connectToServer() throws IOException {
+        displayMessage("Attempting connection\n");
 
-                //display packet contents
-                displayMessage("\nPacket received: " +
-                        "\nFrom host: " + receivePacket.getAddress() +
-                        "\nHost port: " + receivePacket.getPort() +
-                        "\nLength: " + receivePacket.getLength() +
-                        "\nContaining:\n\t " + new String(receivePacket.getData(), 0, receivePacket.getLength()));
-            }catch (IOException ioException){
-                displayMessage(ioException + "\n");
-                ioException.printStackTrace();
+        //create Socket to make connection to server
+        client = new Socket(InetAddress.getByName(chatServer), 12345);
+
+        //display connection information
+        displayMessage("Connect to: " + client.getInetAddress().getHostName());
+    }
+
+    //get streams to send and receive data
+    private void getStreams() throws IOException {
+        //set up output streams for objects
+        output = new ObjectOutputStream(client.getOutputStream());
+        output.flush(); //flush output buffer to send header information
+
+        //set up input stream for objects
+        input = new ObjectInputStream(client.getInputStream());
+        displayMessage("\nGot I/O streams\n");
+    }
+
+    //process connection with server
+    private void processConnection() throws IOException {
+        //enable enterField so client user can send messages
+        setTextEditable(true);
+
+        do { //process messages sent from server
+            try{ //read message and display it
+                message = (String) input.readObject(); //read new message
+                displayMessage("\n" + message);
+            }catch (ClassNotFoundException classNotFoundException){
+                displayMessage("\nUnknown object type received");
             }
+        }while (!message.equals("SERVER>>> TERMINATE"));
+    }
+
+    //close streams and socket
+    private void closeConnection() {
+        displayMessage("\nClosing connection");
+        setTextEditable(false);
+
+        try {
+            output.close();
+            input.close();
+            client.close();
+        }catch (IOException e){
+            e.printStackTrace();
         }
     }
 
-    //manipulates the displayArea in the event-dispatch thread
-    private void displayMessage(final String messageToDisplay) {
+    //send message to server
+    private void sendData(String actionCommand) {
+        try {
+            output.writeObject("CLIENT>>> " + actionCommand);
+            output.flush(); //flush data to output
+            displayMessage("\nClient>>> " + actionCommand);
+        }catch (IOException ioException){
+            displayArea.append("\nError writing object");
+        }
+    }
+
+    //manipulates enterField in the event-dispatch thread
+    private void setTextEditable(final boolean b) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
-            public void run() { //updates displayArea
-                displayArea.append(messageToDisplay);
+            public void run() { //sets enterField's editability
+                enterField.setEditable(true);
             }
         });
     }
+
+    //manipulates displayArea in the event-dispatch thread
+    private void displayMessage(final String s) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() { //updates displayArea
+                displayArea.append(s);
+            }
+        });
+
+    }
+
+
+
+
 }
